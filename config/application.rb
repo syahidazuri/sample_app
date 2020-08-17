583cb69193a97233f3730b1dfe77c20d6fec0f2d
syahidazuri <wnsyahida98@yahoo.com>
2020-08-17 22:16:56 +0800

    Init Project
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything
    
     #Init Project
    
     #Gemfile
    
     #write anything

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**','*.{rb,yml}')]
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
